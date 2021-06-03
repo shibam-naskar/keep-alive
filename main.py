16770bfa3b193bef1b2e5c5e4fe4ebb94311822c
shibam-naskar <shibamnaskar2@gmail.com>
2021-06-03 18:24:23 +0530

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from time import sleep
 import requests
 
 while True:
-    sleep(10)
+    sleep(900)
     r = requests.get("https://ytmusic-uf.herokuapp.com/youtube-data/despacito")
     print(r.status_code)
     
